ae03b3b174c26ba2a98dcf619edc5d2e87a55103
guoyao <guoyao.dev@gmail.com>
2013-10-21 15:10:16 +0800

    finish chapter 5: a common library application completed

---
+++ backbone-learning/webroot/js/views/book.js
@@ -0,0 +1,19 @@
+var app = app || {};
+app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template($('#bookTemplate').html()),
+    events: {
+        'click .delete': 'deleteBook'
+    },
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    },
+    deleteBook: function () {
+        // Delete model
+        this.model.destroy();
+        // Delete view
+        this.remove();
+    }
+});
