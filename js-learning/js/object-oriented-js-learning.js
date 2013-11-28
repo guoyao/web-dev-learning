2b8a408b0092940fe322852802aaa8aa80adf705
guoyao <guoyao.dev@gmail.com>
2013-11-28 20:35:09 +0800

    add object oriented javascript test file

---
+++ js-learning/js/object-oriented-js-learning.js
@@ -0,0 +1,16 @@
+/**
+ * Author: guoyao
+ * Time: 11-28-2013 19:44
+ * Blog: http://www.guoyao.me
+ */
+
+var a = 10;
+
+function f() {
+    var b = a;
+    console.log(b);
+    var a = 1;
+    console.log(a);
+}
+
+f();
