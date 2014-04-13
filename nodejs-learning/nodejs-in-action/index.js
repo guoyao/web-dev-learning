6f0e1538d85a890893a703b0cd4c1e8f848b13bd
guoyao <guoyao.dev@gmail.com>
2014-04-14 00:10:54 +0800

    add more files

--- nodejs-learning/nodejs-in-action/index.js
+++ nodejs-learning/nodejs-in-action/index.js
@@ -1,8 +1,13 @@
-var events = require('events');
-var watcher = new events.EventEmitter();
-watcher.on('message', function(data) {
-    console.log(data);
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./key-cert.pem')
+};
+
+var server = https.createServer(options, function(req, res) {
+    res.writeHead(200);
+    res.end('Hello World\n');
 });
-setInterval(function() {
-    watcher.emit('message', new Date());
-}, 1000);
+server.listen(3000);
