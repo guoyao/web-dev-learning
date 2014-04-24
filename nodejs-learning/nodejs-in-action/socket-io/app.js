b503a54c6f39dbbf0a0d83b1c3f8ba783ab9c1e5
guoyao <guoyao.dev@gmail.com>
2014-04-24 15:55:57 +0800

    add socket

---
+++ nodejs-learning/nodejs-in-action/socket-io/app.js
@@ -0,0 +1,20 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var html = fs.readFileSync('index.html', 'utf8');
+
+function handler(req,res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(html,'utf8'));
+    res.end(html);
+}
+
+function tick() {
+    var now = new Date();
+    io.sockets.send(now.toString());
+    io.sockets.emit('custom-event', now.toString());
+}
+
+setInterval(tick, 1000);
+
+app.listen(3001);
